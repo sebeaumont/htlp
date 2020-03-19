abba7f56bd90a58e3421f4ce6791ee6e14511fba
Simon Beaumont <datalligator@icloud.com>
2020-03-19 21:53:53 +0000

    More type hacks and mind fucks.

--- src/HTLP/MyTypes.hs
+++ src/HTLP/MyTypes.hs
@@ -60,17 +60,42 @@ instance Functor T5 where
   fmap f (T5 a) = T5 $ \b -> a (b . f)
 
 
+-- | The trivial or identity monad
+
+newtype W a = W a deriving Show
+
+instance Functor W where
+  fmap :: (a -> b) -> W a -> W b
+  fmap f (W a) = W (f a)
+
+instance Applicative W where
+  pure a = W a
+  W f <*> W a = W (f a)
+
+instance Monad W where
+  return = pure
+  W a >>= f = f a
+
+{-
+Monad laws:
+return a >>= f == f a                      -- l identity
+m >>= return == m                          -- r identity
+(m >>= f) >>= g == m >>= (f >>= g)
+(m >>= f) >>= g == m >>= (\x -> f x >>= g) -- assoc
+-}
+
+-- | Join in terms of bind
+join :: W (W a) -> W a
+-- join (W a) = a
+join m = m  >>= id
+
+bind :: (a -> W b) -> W a -> W b
+bind f (W a) = f a
+
+
 -- | The continutation monad
 
-cont :: a -> (forall r. (a -> r) -> r)
-cont a = \f -> f a
-
-runCont :: (forall r. (a -> r) -> r) -> a
-runCont f = f id
-
-newtype Cont a = Cont
-  { unCont :: forall r. (a -> r) -> r
-  }
+newtype Cont a = Cont { unCont :: forall r. (a -> r) -> r }
 
 -- | Functor instance for Cont 
 instance Functor Cont where
@@ -82,7 +107,7 @@ instance Functor Cont where
 
 instance Applicative Cont where
   pure :: a -> Cont a
-  pure a = Cont $ cont a
+  pure a = Cont $ \f -> f a 
   -- 
   (<*>) :: Cont (a -> b) -> Cont a -> Cont b
   (Cont f) <*> (Cont a)  = Cont $ \b -> f $ \c -> a $ b . c
@@ -90,5 +115,24 @@ instance Applicative Cont where
 instance Monad Cont where
   return = pure
   (>>=) :: Cont a -> (a -> Cont b) -> Cont b
+  -- look hard at this...
   Cont m >>= f = Cont $ \c -> m $ \a -> unCont (f a) c
+
   
+-- | now the monad transformer for Cont 
+
+newtype ContT m a = ContT { unContT :: forall r. (a -> m r) -> m r }
+
+instance Functor (ContT m) where
+  fmap f (ContT a) = ContT $ \c' -> a $ c' . f
+
+instance Applicative (ContT m) where
+  pure a = ContT $ \f -> f a
+  (<*>) :: ContT m (a -> b) -> ContT m a -> ContT m b
+  (ContT f) <*> (ContT a)  = ContT $ \b -> f $ \c -> a $ b . c
+
+instance (Monad m) => Monad (ContT m) where
+  return = pure
+  (>>=) :: ContT m a -> (a -> ContT m b) -> ContT m b
+  ContT m >>= f = ContT $ \c -> m $ \a -> unContT (f a) c
+
